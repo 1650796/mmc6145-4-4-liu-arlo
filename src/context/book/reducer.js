9e701446d66bfe3b19d07de66eae8ffd361bb68f
catsociety <aliu6876@gmail.com>
2024-06-23 02:52:12 -0400

    46/56 tests passing

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,5 +1,26 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+import {
+  ADD_BOOK,
+  REMOVE_BOOK,
+  SEARCH_BOOKS
+} from './actions'
+
+export default function reducer(prevState, {action, payload}) {
+  switch(action) {
+    case ADD_BOOK:
+      const addFaveBook = [...prevState.favoriteBooks, payload]
+      saveToLocalStorage(addFaveBook)
+      return {...prevState, favoriteBooks: [...prevState.favoriteBooks, payload]}
+    case REMOVE_BOOK:
+      const removeFaveBook = prevState.favoriteBooks.filter(favebook => favebook.id !== payload)
+      saveToLocalStorage(removeFaveBook)
+      return {...prevState, favoriteBooks: removeFaveBook}
+    case SEARCH_BOOKS:
+      return {...prevState, bookSearchResults: payload}
+    default: 
+      return prevState
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
